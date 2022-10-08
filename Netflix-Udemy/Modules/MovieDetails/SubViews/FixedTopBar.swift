187d43dfdb4bbb8ae7037fdb2cc3686f7f65a934
Salma Hassan <salmahassan087@gmail.com>
2022-10-08 08:46:51 +0200

    Add missing functionality at Home View

--- Netflix-Udemy/Modules/MovieDetails/SubViews/FixedTopBar.swift
+++ Netflix-Udemy/Modules/MovieDetails/SubViews/FixedTopBar.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct FixedTopBar: View {
+	@Environment(\.presentationMode) private var presentationMode
+	var closeBtnCompletion: (() -> Void)? = nil
+	
 	var body: some View {
 		HStack {
 			Spacer()
 			Button {
-				//
+				presentationMode.wrappedValue.dismiss()
+				closeBtnCompletion?()
 			} label: {
-				Image(systemName: "xmark.circle")
+				Image(systemName: "xmark.circle.fill")
 					.font(.title2)
 					.foregroundColor(.white)
 			}.buttonStyle(PlainButtonStyle())
